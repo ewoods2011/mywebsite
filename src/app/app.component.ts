18ef9aaf00110accd44319368444339d799d1d14
Edward Woods <ewoods8111@gmail.com>
2019-09-24 09:42:14 -0500

    Test commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'app workss!';
+
+  testFunc = function(){
+    console.log("Hello there");
+  }
 }
